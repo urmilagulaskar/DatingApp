bebbbed7b9187efbad0a4f6bc00d66952b8c2fe9
urmilagulaskar <urmilagulaskar89@gmail.com>
2024-12-17 20:37:00 +0530

    end of section3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -26,7 +27,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization(); */
-
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
