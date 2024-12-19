970282038573507a3c044a7b13e5957914287b6f
urmilagulaskar <urmilagulaskar89@gmail.com>
2024-12-19 11:32:16 +0530

    with parent chile data transfer functionality

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  http = inject(HttpClient);
+  registerMode = false;
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: resp => this.users = resp,
+      error: err => console.log(err),
+      complete: () => console.log('Request has completed')
+    });
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  usersFromHomeComponent = input.required<any>();
+  cancelRegister = output<boolean>();
+  model:any={}
+  register(){
+    console.log(this.model);
+  }
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
